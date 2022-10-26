7f3d2bf282738a38ff01ff3571b375e0da9b437b
ngophuty <ngoputy233@gmail.com>
2022-10-26 16:27:29 +0700

    cookiecutter

--- {{cookiecutter.project_name}}/main.py
+++ {{cookiecutter.project_name}}/main.py
@@ -1,14 +1,23 @@
 from {{cookiecutter.file_env_name}} import env
-
+from fastapi.middleware.wsgi import CORSMiddleware
+from {{cookiecutter.project_name}}.core.middleware.middleware import add_process_time_header
 
 import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    add_process_time_header)
+
+
+
 @app.get('/',tags=['Hello'])
 async def hello_world():
     return {'message':'hello world!'}
+
+
 
 
 if __name__ == "__main__":
